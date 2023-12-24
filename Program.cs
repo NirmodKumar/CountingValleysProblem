d8de7caff5e21e285c9d3c3abb82285c77817a06
Nirmod Kumar <nirmod.kumar656@gmail.com>
2023-12-24 10:52:42 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var steps = 8;
+var path = "UDDDUDUU";
+
+var response = countingValleys(steps, path);
+
+Console.WriteLine(response);
+
+static int countingValleys(int steps, string path)
+{
+    if (steps < 2 || string.IsNullOrWhiteSpace(path) || steps != path.Length)
+    {
+        return 0;
+    }
+    var stepCounter = 0;
+    var valleysCounter = 0;
+
+    for (int i = 0; i < steps; i++)
+    {
+        if (path[i] == 'U')
+        {
+            stepCounter = stepCounter + 1;
+            if (stepCounter == 0)
+            {
+                valleysCounter = valleysCounter + 1;
+
+            }
+        }
+        if (path[i] == 'D')
+        {
+            stepCounter = stepCounter - 1;
+        }
+    }
+
+    return valleysCounter;
+}
+
+
+Console.ReadLine();
